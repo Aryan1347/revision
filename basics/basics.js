3c08b4c6a846d3eec3417d2d7ba63aa29a55fefc
Aryan1347 <gawadearyan2@gmail.com>
2026-02-09 12:11:34 +0530

    Initial commit

---
+++ basics/basics.js
@@ -0,0 +1,13 @@
+// sum of two integers
+let a = 10;
+let b = 20;
+console.log(a+b)
+
+// sum of number and string
+let c = 10;
+let d = "20";
+console.log(c+d); // output: 1020
+console.log(typeof(c+d));
+console.log('the sum of 10 and 20 is ' + a + b); // output: the sum of 10 and 20 is 1020
+console.log('the sum of 10 and 20 is ' + (a + b)); // output: the sum of 10 and 20 is 30
+console.log(a + b + 'this is the sum'); // output: 30this is the sum
